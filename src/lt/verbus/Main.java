3047ad9a0dcb570dd2a837b0c03dedc0a1222f4f
Sarunas Verbus <sarunas@verbus.lt>
2020-07-15 10:05:29 +0200

    completed application testing according to homework task

--- src/lt/verbus/Main.java
+++ src/lt/verbus/Main.java
@@ -3,6 +3,18 @@ package lt.verbus;
 public class Main {
 
     public static void main(String[] args) {
+        //-----Test 1
+        CustomMap<DnsProvider,DnsServer> dnsMap = new CustomMap<>();
+
+        dnsMap.insert(DnsProvider.NETFLIX, new DnsServer("4.0.125.1", "4.0.125.2"));
+        dnsMap.insert(DnsProvider.AMAZON_PRIME, new DnsServer("8.0.0.1", "8.0.0.2"));
+        dnsMap.insert(DnsProvider.HBO, new DnsServer("3.3.5.1", "3.3.5.2"));
+
+        System.out.println(dnsMap);
+        System.out.println();
+        System.out.println(dnsMap.get(DnsProvider.HBO));
+
+        //-----Test 2
         CustomMap<String, String> customMap = new CustomMap<>();
 
         customMap.insert("Vienas", "Pirmas");
