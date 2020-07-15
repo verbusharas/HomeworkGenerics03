fc6a50a6eb0411485080c0fb2fb1f96181827625
Sarunas Verbus <sarunas@verbus.lt>
2020-07-15 09:48:31 +0200

    quick testing of CustomMap class functionality

--- src/lt/verbus/Main.java
+++ src/lt/verbus/Main.java
@@ -3,6 +3,14 @@ package lt.verbus;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CustomMap<String, String> customMap = new CustomMap<>();
+
+        customMap.insert("Vienas", "Pirmas");
+        customMap.insert("Du", "Antras");
+        customMap.insert("Trys", "Trečias");
+        customMap.insert("Keturi", "Ketvirtas");
+
+        System.out.println(customMap);
+        System.out.println(customMap.get("Trys"));
     }
 }
